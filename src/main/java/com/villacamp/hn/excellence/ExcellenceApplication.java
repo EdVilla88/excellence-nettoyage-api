be00d4839b048527ca9b94e100223119bb5747cb
Edwin Villanueva <efvc88@hotmail.com>
2023-09-06 20:15:43 -0600

    initial commit

---
+++ src/main/java/com/villacamp/hn/excellence/ExcellenceApplication.java
@@ -0,0 +1,13 @@
+package com.villacamp.hn.excellence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcellenceNettoyageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcellenceNettoyageApiApplication.class, args);
+	}
+
+}
