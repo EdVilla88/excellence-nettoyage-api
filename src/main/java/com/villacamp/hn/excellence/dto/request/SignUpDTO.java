eb29b6e4e51013b7705f2ba9ba9ad3638f168eb0
Edwin Villanueva <efvc88@hotmail.com>
2023-09-13 19:44:21 -0600

    Update SignUpDTO.java

--- src/main/java/com/villacamp/hn/excellence/dto/request/SignUpDTO.java
+++ src/main/java/com/villacamp/hn/excellence/dto/request/SignUpDTO.java
@@ -1,19 +1,27 @@
 package com.villacamp.hn.excellence.dto.request;
 
 import jakarta.validation.constraints.Email;
-import lombok.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpDTO {
-    @NonNull
+    @NotNull
+    @NotEmpty
     private String fullName;
     @Email
     private String email;
-    @NonNull
+    @NotNull
+    @NotEmpty
     private String password;
-    @NonNull
+    @NotNull
+    @NotEmpty
     private String phone;
 }
